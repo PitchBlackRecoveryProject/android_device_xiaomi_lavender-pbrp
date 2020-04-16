449b2c79600216d389dbf08c515bb1446f76c2a5
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-04-16 17:18:12 +0000

    lavender: switch to kernel source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,15 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
-TARGET_PREBUILT_KERNEL := device/xiaomi/lavender/prebuilt/Image.gz-dtb
+#TARGET_PREBUILT_KERNEL := device/xiaomi/lavender/prebuilt/Image.gz-dtb
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+  TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+  TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
+  TARGET_KERNEL_CONFIG := lavender-perf_defconfig
+endif
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm660
