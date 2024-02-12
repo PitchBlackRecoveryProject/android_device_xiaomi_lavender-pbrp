907e86d3214ff7b352c871dc6b08bb0a43f2d6d3
Amrito Karmokar <amritokarmokar5i@gmail.com>
2024-02-12 13:38:28 +0000

    r5x: Cleanup And Rearrange
    - Also declare fs type
    
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -23,7 +23,6 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
     libxml2 \
     libdisplayconfig.qti \
-    vendor.display.config@1.0 \
     vendor.display.config@2.0
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
