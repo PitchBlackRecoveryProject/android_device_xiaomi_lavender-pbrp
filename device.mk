445a0449c181429d4dafed22ce914c592588a8e5
Amrito Karmokar <amritokarmokar5i@gmail.com>
2024-02-12 12:54:38 +0000

    r5x: Add TW_DEVICE_VERSION

--- device.mk
+++ device.mk
@@ -49,3 +49,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
 PRODUCT_SHIPPING_API_LEVEL := 28
+
+# Add TW_DEVICE_VERSION
+TW_DEVICE_VERSION := by AmritoRock
